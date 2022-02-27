6d7ec7776092a9a9452a7d0a78a3f390ed18e4d0
Kaya0312 <oyama_kayano@yahoo.co.jp>
2022-02-27 02:07:40 -0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,17 @@
+// Require Express to run server and routes
+
+// Start up an instance of app
+
+// Initialize the main project folder
+
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Setup Server
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
---
+++ website/app.js
@@ -0,0 +1,4 @@
+/* Global Variables */
+
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
