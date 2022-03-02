3fc022e8184949d071dd695b32fb2cecba1c963b
Kaya0312 <oyama_kayano@yahoo.co.jp>
2022-03-02 10:39:02 -0800

    feat: add GET and POST functionalities

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const bodyParser = require('body-parser');
 // Start up an instance of app
 const app = express();
 
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
 // Initialize the main project folder
 app.use(express.static('website'));
 
@@ -13,17 +21,17 @@ app.use(express.static('website'));
 // Add a console.log() to the server callback function, and
 // test that your server is running using Node in the terminal to run the file server.js
 
-app.get('/', function (req, res) {
-	res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+// 	res.send('Hello World!');
+// });
 
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // TODO:
 // Add a GET route that returns the projectData object in your server code
-app.get('/projectData', function (req, res) {
-	res.json(projectData);
+app.get('/weather', (req, res) => {
+	res.send(projectData);
 });
 
 // Then, add a POST route that adds incoming data to projectData.
@@ -34,41 +42,21 @@ app.get('/projectData', function (req, res) {
 //// user response
 // Make sure your POST route is setup to add each of
 // these values with a key to projectData.
-app.post('/add', (req, res) => {
-	const allData = req.body;
-	const weatherDatatemperature = allData.temperature;
-
-	const projectDataItem = {
-		temperature: todoTitle,
-		date: new Date(),
-		userResponse: userResponse,
+app.post('/weather/add', (req, res) => {
+	// Receive data from client side
+	projectData = {
+		date: req.body.date,
+		temp: req.body.temp,
+		content: req.body.content,
+		description: req.body.description,
+		city: req.body.city,
 	};
 
-	projectData.push(projectDataItem);
-
-	res.json(projectDataItem);
-});
-
-app.get('/all', function (req, res) {
-	const testData = {
-		temp: 23.567,
-		feel: 'Hot',
-		date: new Date(),
-	};
-
-	res.json(testData);
+	res.send(projectData);
 });
 
 // Setup Server
 app.listen(3000, () => console.log('TEST'));
-
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// Cors for cross origin allowance
-app.use(cors());
 
 // 参考
 // https://sbfl.net/blog/2018/08/25/nodejs-express-webapi/
--- website/app.js
+++ website/app.js
@@ -1,76 +1,147 @@
 // Acquire API credentials from OpenWeatherMap website.
 // Use your credentials and the base url to create global variables
+
 // at the top of your app.js code.
-const apiKey = '03f20d3033382f741229c12ba23e1fc6&units=imperial';
+const apiKey = '03f20d3033382f741229c12ba23e1fc6';
+const baseUrl =
+	'https://api.openweathermap.org/data/2.5/weather?units=imperial&zip=';
 
 /* Global Variables */
 const submitBtn = document.getElementById('generate');
-const zip = document.getElementById('zip');
 const feelings = document.getElementById('feelings');
+const feelValue = feelings.value;
+const zipValue = zip.value;
+const temperature = 5;
+
+let d = new Date();
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
 
 // Write an async function in app.js that uses fetch() to make a GET request to the OpenWeatherMap API.
 
-const getWeatherData = async () => {
+// Inside that callback function call your async GET request with the parameters:
+//// base url
+//// user entered zip code (see input in html with id zip)
+//// personal API key
+const fetchWeather = async (url, zip, api) => {
 	try {
-		const res = await fetch(
-			`https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=${apiKey}`
-		);
+		const res = await fetch(`${url}${zip},us&appid=${api}`);
 		const data = await res.json();
-		console.log('data', data);
+		return data;
 	} catch (error) {
 		console.log('Error: ', error);
 	}
 };
 
-// Create an event listener for the element with the id: generate, with a callback function
-// to execute when it is clicked.
-submitBtn.addEventListener('click', function () {
-	getWeatherData();
-});
-// Inside that callback function call your async GET request with the parameters:
-//// base url
-//// user entered zip code (see input in html with id zip)
-//// personal API key
+// Fetch data based on zip code
+const fetchWeatherByZipCode = async () => {
+	const zip = document.getElementById('zip').value;
+	try {
+		const res = await fetchWeather(baseUrl, zip, apiKey);
+
+		console.log('res.main.temp', res.main.temp);
+		console.log('res.weather[0].description', res.weather[0].description);
+		console.log('res.name', res.name);
+
+		postData({
+			date: newDate,
+			content: feelValue,
+			temp: res.main.temp,
+			description: res.weather[0].description,
+			city: res.name,
+		});
+
+		updateUI();
+	} catch (error) {
+		console.log('Error: ', error);
+	}
+};
+
+const generateJournal = async () => {};
+
+// async function postNewItem(data) {
+// 	try {
+// 		const feelValue = feelings.value;
+// 		const zipValue = zip.value;
+// 		const temperature = test.main.temp;
+
+// 		if (zipValue.length === 0 || feelValue.length === 0) return;
+
+// 		const data = {
+// 			date: newDate,
+// 			temp: temperature,
+// 			content: feelValue,
+// 		};
+
+// 		console.log('data?', data);
+// 		updateData(data);
+// 	} catch (error) {
+// 		console.log('Error: ', error);
+// 	}
+
+// 	// .then((response) => response.json());
+// }
 
 // After your successful retrieval of the weather data,
 // you will need to chain another Promise that makes a POST request to add the API data,
 // as well as data entered by the user, to your app.
+const postData = async (data) => {
+	// You will need to write another async function to make this POST request.
+	// The function should receive a path and a data object.
+	// The data object should include
+	//// temperature
+	//// date
+	//// user response
+	// Remember, you can access the value of DOM elements by selecting them in your JS code.
 
-// You will need to write another async function to make this POST request.
-// The function should receive a path and a data object.
-// The data object should include
-//// temperature
-//// date
-//// user response
-// Remember, you can access the value of DOM elements by selecting them in your JS code.
+	if (zipValue.length === 0 || feelValue.length === 0) return;
+
+	console.log('data in postData', data);
+
+	const res = await fetch('/weather/add', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	});
+
+	// console.log('response.json()', await res.json());
+	// console.log('res', res);
+
+	let newData = await res.json();
+	console.log('newData', newData);
+	return newData;
+};
 
 // Finally, chain another Promise that updates the UI dynamically
 // Write another async function that is called after the completed POST request.
-// This function should retrieve data from our app,
-// select the necessary elements on the DOM (index.html),
-// and then update their necessary values to reflect the dynamic values for:
-//// Temperature
-//// Date
-//// User input
-const retrieveData = async () => {
-	const request = await fetch('/all');
-	console.log('request', request);
+const updateUI = async () => {
+	// This function should retrieve data from our app,
+	const request = await fetch('/weather');
+
 	try {
 		// Transform into JSON
-		const allData = await request.json();
-		console.log('allData', allData);
-		// 	// Write updated data to DOM elements
+		const data = await request.json();
+
+		// select the necessary elements on the DOM (index.html),
+		// and then update their necessary values to reflect the dynamic values for:
+		//// Temperature
+		//// Date
+		//// User input
 		document.getElementById('temp').innerHTML =
-			Math.round(allData.temp) + 'degrees';
-		document.getElementById('content').innerHTML = allData.feel;
-		document.getElementById('date').innerHTML = allData.date;
+			Math.round(data.temp) + 'degrees';
+		document.getElementById('date').innerHTML = data.date;
+		document.getElementById('content').innerHTML = data.content;
+		document.getElementById('city').innerHTML = data.city;
+		document.getElementById('overall').innerHTML = data.description;
 	} catch (error) {
 		console.log('error', error);
-		// appropriately handle the error
 	}
 };
-retrieveData();
 
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// Create an event listener for the element with the id: generate, with a callback function
+// to execute when it is clicked.
+// submitBtn.addEventListener('click', fetchWeatherByZipCode);
+submitBtn.addEventListener('click', fetchWeatherByZipCode);
